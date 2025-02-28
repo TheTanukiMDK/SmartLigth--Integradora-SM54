76d89b82090fe4b9390c7907564139d904926828
TheTanukiMDK <riquicraft04@gmail.com>
2025-02-28 10:38:25 -0500

    Integradora Proyect

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { Ionicons } from "@expo/vector-icons"; // Importa los íconos
 
-export default function App() {
+const LoginScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient colors={["#9b59b6", "#512DA8"]} style={styles.container}>
+      <View style={styles.logoContainer}>
+      <Ionicons name="home" size={100} color="white" />
+      </View>
+      <View style={styles.card}>
+        <Text style={styles.title}>Inicio de sesión</Text>
+        <TextInput style={styles.input} placeholder="Correo Electrónico" />
+        <TextInput style={styles.input} placeholder="Contraseña" secureTextEntry />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Iniciar sesión</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.registerText}>Registrarse</Text>
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logoContainer: {
+    marginBottom: 20,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    tintColor: "white",
+  },
+  card: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    width: "85%",
+    alignItems: "center",
+    elevation: 5,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 15,
+  },
+  input: {
+    width: "100%",
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "#6A1B9A",
+    paddingVertical: 10,
+    borderRadius: 5,
+    width: "100%",
+    alignItems: "center",
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  registerText: {
+    color: "#2979FF",
+    marginTop: 10,
   },
 });
+
+export default LoginScreen;
